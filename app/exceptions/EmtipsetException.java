46fa123471e7fad2bfd485570382d81ca369e4b6
Gösta J <gostaj@gmail.com>
2012-05-25 00:23:48 +0200

    Added our own RuntimeException (EmtipsetException).

---
+++ app/exceptions/EmtipsetException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+/**
+ * Used for EM-tipset specific exceptions.
+ */
+public class EmtipsetException extends RuntimeException {
+    public EmtipsetException(String msg) {
+        super(msg);
+    }
+}
