2aa3af959ab6ed7c2073d4df82aff6c4103f943c
Delagott <->
2012-05-30 11:18:57 +0200

    Final (?) polish on info box.

--- public/javascripts/europe.js
+++ public/javascripts/europe.js
@@ -45,20 +45,11 @@ function getCountryInfo(d, data) {
         d3.select("div#countryinfo div#flag img").attr("src", "public/images/" + d.properties.GU_A3 + ".png").attr("alt", d.properties.GU_A3);
         d3.select("div#countryinfo div#info").text("");
         d3.select("div#countryinfo div#info").append("p").attr("id", "fifa_rank").text("Fifa-ranking: " + data.q[d.properties.GU_A3].fifa_rank);
-        d3.select("div#countryinfo div#info").append("p").attr("id", "group").text("Grupp " + data.q[d.properties.GU_A3].group);
+        d3.select("div#countryinfo div#info").append("p").attr("id", "group").text("Grupp: ").append("span").attr("class", "group_letter").attr("id", "grp_" + data.q[d.properties.GU_A3].group).text(data.q[d.properties.GU_A3].group);
         d3.select("div#countryinfo div#info").append("p").attr("id", "odds").text("Odds för vinst: " + data.q[d.properties.GU_A3].odds + " (Unibet)"); 
         d3.select("div#countryinfo div#info").append("p").attr("id", "odds_14").text("Odds för placering 1-4: " + data.q[d.properties.GU_A3].odds_14 + " (Unibet)");
-        var status;
-        switch(data.q[d.properties.GU_A3].status) {
-            case "active": status = ""; break;
-            case "elim_grp": status = "Utslagna i gruppspelet"; break;
-            case "elim_quarter": status = "Utslagna i kvartsfinal"; break;
-            case "elim_semi": status = "Utslagna i semi-final"; break;
-            case "win_3rd" : status = "Vinnare av EM-brons!"; break;
-            case "elim_final": status = "Vinnare av EM-silver!"; break;
-            case "win_final": status = "Vinnare av EM-guld!"; break;
-        }
-        d3.select("div#countryinfo div#info").append("p").attr("id", "status").text(status);
+        d3.select("div#countryinfo div#info").append("p").attr("id", "qualified").text("Väg till EM: " + data.q[d.properties.GU_A3].qual);
+        d3.select("div#countryinfo div#info").append("p").attr("id", "other").text(data.q[d.properties.GU_A3].other);
        
     } else if (data.dnq.indexOf(d.properties.GU_A3) != -1) {
         d3.select("div#countryinfo div#flag img").attr("src", "public/images/" + d.properties.GU_A3 + "-bw.png").attr("alt", d.properties.GU_A3);
