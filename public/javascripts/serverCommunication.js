c8399fde91d5a28482380c6da837f3e845919795
Gösta J <gostaj@gmail.com>
2012-06-01 00:25:06 +0200

    Added JS functions for disabling all radio buttons and for getting results and showing the results on the page.

--- public/javascripts/serverCommunication.js
+++ public/javascripts/serverCommunication.js
@@ -9,11 +9,10 @@ var request = $.ajax({
 });
 
 request.fail(function(jqXHR, textStatus) {
-  alert( "Problem att kontakta servern, prova att tippa igen eller logga ut och in igen. \nFel: " + textStatus);
+  alert( "Problem att kontakta servern (tippa), prova att tippa igen eller logga ut och in igen. \nFel: " + textStatus);
 });
 
 }
-
 
 
 function getGameBets() {
@@ -25,7 +24,39 @@ var request = $.getJSON('/gamebets', function(data) {
 });
 
 request.fail(function(jqXHR, textStatus) {
-  alert( "Problem att kontakta servern, prova att ladda om sidan eller logga ut och in igen. \nFel: " + textStatus);
+  alert( "Problem att kontakta servern (hämta tippningar), prova att ladda om sidan eller logga ut och in igen. \nFel: " + textStatus);
 });
 
+}
+
+function getResults() {
+
+var request = $.getJSON('/results', function(data) {
+  var userPoints = 0;
+  var maxPoints = 0;
+  $.each(data, function(key, gameBet) {
+    var resultRadio = $("#game_" + gameBet.gameId + "_" + gameBet.result);
+    if (resultRadio.attr("checked") == "checked") {
+        resultRadio.closest("td").addClass("right");
+        userPoints++;
+    } else {
+        resultRadio.closest("td").addClass("wrong");
+    }
+    maxPoints++;
+  });
+
+  // Print out userPoints and maxPoints
+  $("#userPoints").text(userPoints);
+  $("#maxPoints").text(maxPoints);
+
+});
+
+request.fail(function(jqXHR, textStatus) {
+  alert( "Problem att kontakta servern (hämta resultat), prova att ladda om sidan eller logga ut och in igen. \nFel: " + textStatus);
+});
+
+}
+
+function disableBetting() {
+  $(':radio').attr('disabled',true);
 }
